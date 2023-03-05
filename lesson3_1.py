44ac34df421acdff0b31c7b36b341383eb97fd81
Khasanov Fidail <khasanovfsh@gmail.com>
2023-03-05 16:19:41 +0300

    initial

---
+++ lesson3_1.py
@@ -0,0 +1,28 @@
+while True:
+	comand = input('Выберите операцию: ')
+	if comand in '+-/*':
+		break
+	print('Ошибка ввода. Такой команды не существует. Попробуйте еще раз.')
+
+count = 1
+operand_amound = int(input('Введите количество операндов: '))
+number = int(input(f'Введите число {count}: '))
+result = number
+result_string = str(number)
+
+while operand_amound != 1:
+	operand_amound -= 1
+	count += 1
+	number = int(input(f'Введите число {count}: '))
+
+	if number != 0:
+		if comand == '+':
+			result += number
+		if comand =='-':
+			result -= number
+		if comand == '*':
+			result *= number
+		if comand == '/':
+			result /= number
+	result_string += " " + comand + " " + str(number)
+print(result_string + " = " + str(result))
