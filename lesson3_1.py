513960db913e78137e93480a00132580c929b03f
Khasanov Fidail <khasanovfsh@gmail.com>
2023-03-09 19:15:13 +0300

    change title and input is lesson3_1.py

--- lesson3_1.py
+++ lesson3_1.py
@@ -5,7 +5,7 @@ while True:
 	print('Ошибка ввода. Такой команды не существует. Попробуйте еще раз.')
 
 count = 1
-operand_amound = int(input('Введите количество операндов: '))
+operand_amound = int(input('Введите количество слогаемых: '))
 number = int(input(f'Введите число {count}: '))
 result = number
 result_string = str(number)
@@ -20,5 +20,7 @@ while operand_amound != 1:
 			result -= number
 		if comand == '*':
 			result *= number
+		if comand == '9':
+			result += 9
 	result_string += " " + comand + " " + str(number)
 print(result_string + " = " + str(result))
